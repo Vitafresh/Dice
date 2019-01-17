f889fd2453f9693abb09c96b178d26ae619c68e4
Vitafresh <vvitafresh@gmail.com>
2019-01-17 23:13:42 +0200

    Lesson 35 (Using Variables and Linking Layout)

--- app/src/main/java/top/vitafresh/dice/MainActivity.java
+++ app/src/main/java/top/vitafresh/dice/MainActivity.java
@@ -2,6 +2,8 @@ package top.vitafresh.dice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_roll;
+        btn_roll = (Button) findViewById(R.id.btn_roll);
+
+        ImageView imgLeftDice, imgRightDice;
+        imgLeftDice = (ImageView) findViewById(R.id.img_leftDice);
+        imgRightDice=(ImageView) findViewById(R.id.img_rightDice);
+        
+
+
+
     }
 }
